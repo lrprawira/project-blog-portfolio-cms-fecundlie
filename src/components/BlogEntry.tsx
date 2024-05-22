9150f6d6d5ce6cc02cfcdb533fe46d84a945fe17
Louis <deck@louis-deck.lan>
2024-05-22 23:54:24 +0700

    Adjust BlogEntry to handle all nullish values on title, content and timestamp

--- src/components/BlogEntry.tsx
+++ src/components/BlogEntry.tsx
@@ -1,12 +1,14 @@
-import { Component } from "solid-js"
+import { Component, Show } from "solid-js"
 
-const BlogEntry: Component<{ title: string, content: string, timestamp: number }> = (props) => {
+const BlogEntry: Component<{ title?: string, content?: string, timestamp?: number }> = (props) => {
 	return (
 		<div class="w-full text-sm">
-			<h1 class="text-2xl text-slate font-bold uppercase">{props.title}</h1>
-			<div class="text-base font-light" innerHTML={props.content}></div>
+			<h1 class="text-2xl text-slate font-bold uppercase">{props.title ?? 'Untitled'}</h1>
+			<div class="text-base font-light" innerHTML={props.content ?? '(no content)'}></div>
 			<div class="mt-8 text-gray-400">
-				{new Date(props.timestamp * 1000).toDateString()}
+				<Show when={props.timestamp} fallback={'Invalid Date'}>
+					{new Date((props.timestamp!) * 1000).toDateString()}
+				</Show>
 			</div>
 		</div>
 	);
