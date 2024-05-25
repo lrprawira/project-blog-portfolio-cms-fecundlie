ccbd8ffe5eb5353bbc5f052b364de54e9328664d
Louis <deck@louis-deck.lan>
2024-05-25 17:33:31 +0700

    Autoformat uno.config.ts

--- uno.config.ts
+++ uno.config.ts
@@ -1,18 +1,16 @@
-import { defineConfig, presetWebFonts, transformerDirectives } from 'unocss';
-import { presetUno } from 'unocss';
+import { defineConfig, presetWebFonts, transformerDirectives } from "unocss";
+import { presetUno } from "unocss";
 
 export default defineConfig({
-	presets: [
-		presetUno(),
-		presetWebFonts({
-			provider: 'bunny',
-			fonts: {
-				abeezee: 'ABeeZee',
-				inter: 'Inter:300,400,700',
-			}
-		}),
-	],
-	transformers: [
-		transformerDirectives(),
-	]
-})
+  presets: [
+    presetUno(),
+    presetWebFonts({
+      provider: "bunny",
+      fonts: {
+        abeezee: "ABeeZee",
+        inter: "Inter:300,400,700",
+      },
+    }),
+  ],
+  transformers: [transformerDirectives()],
+});
